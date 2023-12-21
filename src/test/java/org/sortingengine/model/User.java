fab5bf0206de80dbd81d791e69939fa3132a742d
Rabi <rabinarayan.patra@thoughtclan.com>
2023-12-21 22:49:20 +0530

    added java doc

--- src/test/java/org/sortingengine/model/User.java
+++ src/test/java/org/sortingengine/model/User.java
@@ -2,6 +2,24 @@ package org.sortingengine.model;
 
 import java.util.Objects;
 
+import org.sortingengine.algo.SortingEngine;
+
+/**
+ * The User class represents a simple user entity with an ID and a name. It is designed to be used for testing various
+ * sorting algorithms within the SortingEngine class. The User class provides necessary fields and methods to facilitate
+ * sorting based on different attributes like ID or name.
+ *
+ * <p>This class includes:
+ * <ul>
+ *     <li>An integer ID field to uniquely identify a user.</li>
+ *     <li>A String name field representing the user's name.</li>
+ * </ul>
+ *
+ * <p>Additionally, the class overrides equals and hashCode methods to ensure
+ * proper comparison and functioning in collections.</p>
+ *
+ * @see SortingEngine
+ */
 public class User {
   private final int id;
   private final String name;
