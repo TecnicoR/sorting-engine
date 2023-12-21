809ea8b81ec1e6a9db62fe94d8e3ab1511cf250b
Rabi <rabinarayan.patra@thoughtclan.com>
2023-12-21 22:59:42 +0530

    Converted User from class to record.

--- src/test/java/org/sortingengine/model/User.java
+++ src/test/java/org/sortingengine/model/User.java
@@ -1,55 +1,23 @@
 package org.sortingengine.model;
 
-import java.util.Objects;
-
 import org.sortingengine.algo.SortingEngine;
 
 /**
- * The User class represents a simple user entity with an ID and a name. It is designed to be used for testing various
- * sorting algorithms within the SortingEngine class. The User class provides necessary fields and methods to facilitate
- * sorting based on different attributes like ID or name.
+ * The User record represents a simple user entity with an ID and a name. It is designed to be used for testing various
+ * sorting algorithms within the SortingEngine class. As a record, User provides an efficient and concise way to
+ * represent immutable data, ideal for sorting based on different attributes like ID or name.
  *
- * <p>This class includes:
+ * <p>Attributes of the User record:</p>
  * <ul>
- *     <li>An integer ID field to uniquely identify a user.</li>
- *     <li>A String name field representing the user's name.</li>
+ *     <li>{@code int id}: An integer ID field to uniquely identify a user.</li>
+ *     <li>{@code String name}: A String field representing the user's name.</li>
  * </ul>
  *
- * <p>Additionally, the class overrides equals and hashCode methods to ensure
- * proper comparison and functioning in collections.</p>
+ * <p>Since User is defined as a record, it automatically provides implementations of
+ * {@code equals()}, {@code hashCode()}, and {@code toString()} methods that are based on
+ * the record's state.</p>
  *
  * @see SortingEngine
  */
-public class User {
-  private final int id;
-  private final String name;
-
-  public User( int id, String name ) {
-    this.id = id;
-    this.name = name;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  // Override equals and hashCode for proper comparison in tests
-  @Override
-  public boolean equals( Object o ) {
-    if( this == o )
-      return true;
-    if( o == null || getClass() != o.getClass() )
-      return false;
-    User user = (User) o;
-    return id == user.id && Objects.equals( name, user.name );
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash( id, name );
-  }
+public record User( int id, String name ) {
 }
